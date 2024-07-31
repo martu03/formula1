aeff4140472fd061a90e7b03dc576197f6c6c486
martu03 <164392379+martu03@users.noreply.github.com>
2024-07-31 23:50:48 +0200

    Add files via upload

---
+++ Giocatori/IGiocatore.java
@@ -0,0 +1,57 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2024 Martina Colibazzi
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package it.unicam.cs.formula1.api.Giocatori;
+
+import it.unicam.cs.formula1.api.Circuito.ICircuito;
+import it.unicam.cs.formula1.api.Posizione.IPosizione;
+
+public interface IGiocatore {
+
+    /**
+     * Ritorna il simbolo associato al giocatore.
+     *
+     * @return il simbolo del giocatore.
+     */
+    char getSimbolo();
+
+    /**
+     * Ritorna la posizione attuale del giocatore.
+     *
+     * @return la posizione attuale del giocatore.
+     */
+    IPosizione getPosizioneAttuale();
+
+    /*
+     * Determina la prossima mossa del giocatore.
+     *
+     * @param circuito
+     * @param posizioneAttuale del giocatore
+     * @return la prossima mossa del giocatore.
+     */
+    IPosizione ProssimaMossa(ICircuito circuito, IPosizione posizioneAttuale);
+}
